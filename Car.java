c89ae2e68ffd8f82748afe723f7a61df48bb2647
hshams0817 <100087801+hshams0817@users.noreply.github.com>
2022-02-20 12:14:12 -0500

    Add files via upload

---
+++ Car.java
@@ -0,0 +1,20 @@
+package org.javaPackage;
+
+public class Car {
+void startEngine()
+{
+	System.out.println("Start Engine");
+}
+void stopEngine()
+{
+System.out.println("Stop Engine");
+}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Car key = new Car ();
+key.startEngine();
+
+key.stopEngine();
+	}
+
+}
